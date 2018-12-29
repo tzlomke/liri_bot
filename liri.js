969af1c367e9d0d169049afb2af8016a69767cfb
Chieftjz <taylor.zlomke@gmail.com>
2018-12-28 20:55:47 -0700

    installed/required packages

--- liri.js
+++ liri.js
@@ -1,1 +1,14 @@
+// Required Packages
 require("dotenv").config();
+var axios = require("axios");
+var moment = require("moment");
+var spotifyApi = require("node-spofity-api");
+var fs = require("fs");
+var keys = require("keys.js");
+
+// Spotify Keys
+var spotify = new Spotify(keys.spotify);
+
+// Command Line Arguments
+var command = process.argv[2];
+var keyword = process.argv.slice(3).join(" ");
