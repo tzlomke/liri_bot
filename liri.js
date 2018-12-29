5c2e13f9db1d25e240edac86cd8924c8da515b32
Chieftjz <taylor.zlomke@gmail.com>
2018-12-28 21:59:23 -0700

    wrote bandsintown function

--- liri.js
+++ liri.js
@@ -2,9 +2,9 @@
 require("dotenv").config();
 var axios = require("axios");
 var moment = require("moment");
-var spotifyApi = require("node-spofity-api");
+var spotifyApi = require("node-spotify-api");
 var fs = require("fs");
-var keys = require("keys.js");
+var keys = require("./keys");
 
 // Spotify Keys
 var spotify = new Spotify(keys.spotify);
@@ -12,3 +12,53 @@ var spotify = new Spotify(keys.spotify);
 // Command Line Arguments
 var command = process.argv[2];
 var keyword = process.argv.slice(3).join(" ");
+
+// Switch Statements
+// Does this need to be wrapped in a start function???
+function start() {
+    switch (command) {
+        case "concert-this":
+            concertThis();
+            break;
+        
+        case "spotify-this-song":
+            spofityThisSong();
+            break;
+
+        case "movie-this":
+            movieThis();
+            break;
+
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+    };
+}
+
+function concertThis() {
+    axios.get("https://rest.bandsintown.com/artists/" + keyword + "/events?app_id=6d9b15f09f67304fbd702249a8b58714")
+    .then(function(response) {
+        response.data.forEach(gig => {
+            var gigData = "Venue: " + gig.venue.name 
+                + "\nLocation: " + gig.venue.city
+                + "\nDate: " + moment(gig.datetime).format("MM/DD/YYYY")
+                + "\n===============================";
+
+            console.log(gigData);
+        });
+    });
+};
+
+function spofityThisSong() {
+
+}
+
+// function movieThis() {
+
+// }
+
+// function doWhatItSays() {
+
+// }
+
+start();
